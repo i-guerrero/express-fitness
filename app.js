e9a6ea8a70a46edaae2a8672f65d634e25ae878f
i-guerrero <isaacguerrero@pursuit.org>
2022-12-12 15:38:15 -0500

    Add locations, machines, and persons controllers

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // DEPENDENCIES
 const express = require("express");
+const locationsController = require("./controllers/locations.controller.js");
+const machinesController = require("./controllers/machines.controller");
+const personsController = require("./controllers/persons.controller");
 
 // CONFIGURATION
 const app = express();
@@ -8,6 +11,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use("/locations", locationsController);
+
+app.use("/machines", machinesController);
+
+app.use("/persons", personsController);
 
 // EXPORT
 module.exports = app;
