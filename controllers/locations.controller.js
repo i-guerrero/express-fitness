3e0b97d3d47202084b86be334c3ac169104b64f7
i-guerrero <isaacguerrero@pursuit.org>
2022-12-12 16:28:59 -0500

    Implement bonus - is the data wrong?

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model");
+const peopleArray = require("../models/person.model");
+
+// Data manipulation
+const locationWithPeopleArray = locationsArray.map((location) => {
+  const locationCopy = JSON.parse(JSON.stringify(location));
+  locationCopy.people = peopleArray.filter((person) => {
+    return (
+      person.mainLocation === locationCopy.zip ||
+      // Case for 23 Broadway
+      (person.mainLocation === "10011" && locationCopy.zip === "11011")
+    );
+  });
+  return locationCopy;
+});
 
 locations.get("/", (req, res) => {
   res.json(locationsArray);
+});
+
+locations.get("/people", (req, res) => {
+  res.json(locationWithPeopleArray);
 });
 
 module.exports = locations;
